662d51deae69262ddeecd17ce15007512cc8e117
João Vitor <169055213+joaov7r@users.noreply.github.com>
2026-09-24 14:40:03 -0300

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -1,1 +1,55 @@
+const estabelecimento = {
 
+    nome: "Barbearia Dom Roger",
+
+    whatsapp: "https://wa.me/5511988734659?text=Ol%C3%A1!%20Vim%20pelo%20site%20da%20Barbearia%20Dom%20Roger%20e%20gostaria%20de%20agendar%20um%20hor%C3%A1rio.",
+
+    instagram: "https://www.instagram.com/barbeariadom.roger/",
+
+    agendamento: "https://cashbarber.com.br/barbeariadomroger/inicio",
+
+    unidades: [
+
+        {
+            nome: "Jardim da Glória",
+
+            endereco: "Av. João Paulo Ablas, 45 - Lj 26 - Jardim da Gloria, Cotia - SP",
+
+            maps: "https://www.google.com/maps/dir/?api=1&destination=Av.+João+Paulo+Ablas,+45+-+Lj+26,+Jardim+da+Gloria,+Cotia+-+SP,+06711-250",
+
+            google: "https://search.google.com/local/writereview?placeid=ChIJHXMq4pmrz5QRXBABiIJ_vms"
+        },
+
+        {
+            nome: "Matriz",
+
+            endereco: "Av. Eid Mansur, 803 - Parque Sao George, Cotia - SP",
+
+            maps: "https://www.google.com/maps/dir/?api=1&destination=Av.+Eid+Mansur,+803,+Parque+Sao+George,+Cotia+-+SP",
+
+            google: "https://search.google.com/local/writereview?placeid=ChIJB-MjKwCrz5QR3dY_ddG2Nng"
+        },
+
+        {
+            nome: "Vianna Village",
+
+            endereco: "R. Mesopotâmia, 109 - Sl 10 - Jardim Passargada I, Cotia - SP",
+
+            maps: "https://www.google.com/maps/dir/?api=1&destination=Rua+Mesopotamia,+109+-+Sl+10,+Jardim+Passargada+I,+Cotia+-+SP",
+
+            google: "https://search.google.com/local/writereview?placeid=ChIJ-wvg1QWrz5QRJvcvKOGjQck"
+        },
+
+        {
+            nome: "Capuava",
+
+            endereco: "Estr. do Capuava, 4421 - Paisagem Renoir, Cotia - SP",
+
+            maps: "https://www.google.com/maps/dir/?api=1&destination=Estrada+do+Capuava,+4421+-+Paisagem+Renoir,+Cotia+-+SP",
+
+            google: "https://search.google.com/local/writereview?placeid=ChIJH8PqU9Crz5QReQl_Gn1aRiM"
+        }
+
+    ]
+
+};
