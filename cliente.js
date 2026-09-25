6e8200b3d2b108da70d7957141b07ccce32749b6
João Vitor <169055213+joaov7r@users.noreply.github.com>
2026-09-25 00:06:04 -0300

    Update cliente.js

--- cliente.js
+++ cliente.js
@@ -4,3 +4,18 @@ const clienteSelecionado =
     params.get("cliente") || "dom-roger";
 
 const estabelecimento = clientes[clienteSelecionado];
+
+document.title = estabelecimento.nome;
+
+document.querySelector(".logo img").src = estabelecimento.logo;
+document.querySelector(".logo img").alt = estabelecimento.nome;
+
+document.getElementById("description").innerHTML =
+    estabelecimento.descricao;
+
+document.body.style.setProperty(
+    "--cliente-fundo",
+    `url("${estabelecimento.fundo}")`
+);
+
+document.body.style.visibility = "visible";
